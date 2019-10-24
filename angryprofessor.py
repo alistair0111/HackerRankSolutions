3b0cdec5da564017d2f926252df202c958c53f24
Alistair Baretto <alistair.baretto@gmail.com>
2019-10-24 10:48:31 +0530

    Create angryprofessor.py

---
+++ angryprofessor.py
@@ -0,0 +1,43 @@
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                       #!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the angryProfessor function below.
+def angryProfessor(k, a):
+    arrived_late=0
+    arrived_ontime=0
+    for i in range(len(a)):
+        if(a[i]<=0):
+            arrived_ontime+=1
+        elif(a[i]>0):
+            arribed_late+=1
+    if(arrived_ontime>=k):
+        return 'YES'
+    esle:
+    
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nk = input().split()
+
+        n = int(nk[0])
+
+        k = int(nk[1])
+
+        a = list(map(int, input().rstrip().split()))
+
+        result = angryProfessor(k, a)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
