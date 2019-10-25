951ccdf313dec347ffda3d9427b687dab752f7d4
Alistair Baretto <alistair.baretto@gmail.com>
2019-10-25 14:40:28 +0530

    Create save_prisoner.py

---
+++ save_prisoner.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the saveThePrisoner function below.
+def saveThePrisoner(n, m, s):
+    sad_prisoner=(s+m-1)%n
+    if(sad_prisoner==0):
+        sad_prisoner=n
+    return sad_prisoner
+
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nms = input().split()
+
+        n = int(nms[0])
+
+        m = int(nms[1])
+
+        s = int(nms[2])
+
+        result = saveThePrisoner(n, m, s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
