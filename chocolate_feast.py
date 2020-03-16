1fd3bfbe26d749b5eb24f940a54a378d516e593f
Alistair Baretto <alistair.baretto@gmail.com>
2020-03-16 20:50:06 +0530

    Update chocolate_feast.py

--- chocolate_feast.py
+++ chocolate_feast.py
@@ -8,13 +8,14 @@ import sys
 
 # Complete the chocolateFeast function below.
 def chocolateFeast(n, c, m):
+    wrap = 0
     num_choc = 0
     num_choc = int(n/c)
     wrap = num_choc
     while wrap>=m:
         temp_wrap = int(wrap/m)
         num_choc+=temp_wrap
-        wrap = num_wrap + (wrap%m)
+        wrap = temp_wrap + (wrap%m)
     return num_choc
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
