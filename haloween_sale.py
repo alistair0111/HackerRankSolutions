d2ef1413a6620ee0ecdb629c66cbea640370d88a
Alistair Baretto <alistair.baretto@gmail.com>
2020-03-26 23:23:30 +0530

    Create haloween_sale.py

---
+++ haloween_sale.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the howManyGames function below.
+def howManyGames(p, d, m, s):
+    # Return the number of games you can buy
+    count = 0
+    while s>=p:
+        count+=1
+        s-=p
+        p = max(p-d,m)
+    return count
+        
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    pdms = input().split()
+
+    p = int(pdms[0])
+
+    d = int(pdms[1])
+
+    m = int(pdms[2])
+
+    s = int(pdms[3])
+
+    answer = howManyGames(p, d, m, s)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
