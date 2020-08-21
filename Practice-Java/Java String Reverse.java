9b147d57e988b7a5ada5a43dde206258361e6456
Alistair Baretto <alistair.baretto@gmail.com>
2020-08-22 01:56:44 +0530

    Create Java String Reverse.java

---
+++ Practice-Java/Java String Reverse.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        int flag = 0;
+        /* Enter your code here. Print output to STDOUT. */
+        for(int i =0;i<=A.length()/2;i++){
+            if(A.charAt(i) == A.charAt(A.length()-i-1)){
+                flag = 1;
+            }
+            else{
+                System.out.println("No");
+                break;
+            }
+        }
+        if(flag == 1){
+            System.out.println("Yes");
+        }
+        
+    }
+}
+
+
+
